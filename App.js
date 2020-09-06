b0199b4483f7cb9bb76aa58d71d2f67a6afbcd9c
Doni Setiawan <donisetiawan.050@gmail.com>
2020-09-06 19:25:57 +0700

    The message list

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Alert,
+  TouchableHighlight,
+  Image,
+  BackHandler,
+} from 'react-native';
 import Status from './components/Status';
+import {
+  createImageMessage,
+  createLocationMessage,
+  createTextMessage,
+} from './utils/MessageUtils';
+import MessageList from './components/MessageList';
 
 const styles = StyleSheet.create({
   container: {
@@ -20,11 +33,105 @@ const styles = StyleSheet.create({
     borderTopColor: 'rgba(0,0,0,0.04)',
     backgroundColor: 'white',
   },
+  fullscreenOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'black',
+    zIndex: 2,
+  },
+  fullscreenImage: {
+    flex: 1,
+    resizeMode: 'contain',
+  },
 });
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: [
+        createImageMessage('https://unsplash.it/300/300'),
+        createTextMessage('World'),
+        createTextMessage('Hello'),
+        createLocationMessage({
+          latitude: 37.78825,
+          longitude: -122.4324,
+        }),
+      ],
+      fullscreenImageId: null,
+    };
+  }
+
+  // eslint-disable-next-line camelcase
+  UNSAFE_componentWillMount() {
+    this.subscription = BackHandler.addEventListener(
+      'hardwareBackPress',
+      () => {
+        const { fullscreenImageId } = this.state;
+
+        if (fullscreenImageId) {
+          this.dismissFullscreenImage();
+          return true;
+        }
+
+        return false;
+      },
+    );
+  }
+
+  componentWillUnmount() {
+    this.subscription.remove();
+  }
+
+  dismissFullscreenImage = () => {
+    this.setState({ fullscreenImageId: null });
+  };
+
+  handlePressMessage = ({ id, type }) => {
+    switch (type) {
+      case 'text':
+        Alert.alert(
+          'Delete message?',
+          'Are you sure you want to permanently delete this message?',
+          [
+            {
+              text: 'Cancel',
+              style: 'cancel',
+            },
+            {
+              text: 'Delete',
+              style: 'destructive',
+              onPress: () => {
+                const { messages } = this.state;
+                this.setState({
+                  messages: messages.filter(
+                    (message) => message.id !== id,
+                  ),
+                });
+              },
+            },
+          ],
+        );
+        break;
+      case 'image':
+        this.setState({ fullscreenImageId: id });
+        break;
+      default:
+        break;
+    }
+  };
+
   renderMessageList() {
-    return <View style={styles.content} />;
+    const { messages } = this.state;
+
+    return (
+      <View style={styles.content}>
+        <MessageList
+          messages={messages}
+          onPressMessage={this.handlePressMessage}
+        />
+      </View>
+    );
   }
 
   renderInputMethodEditor() {
@@ -35,6 +142,32 @@ export default class App extends React.Component {
     return <View style={styles.toolbar} />;
   }
 
+  renderFullscreenImage = () => {
+    const { messages, fullscreenImageId } = this.state;
+
+    if (!fullscreenImageId) return null;
+
+    const image = messages.find(
+      (message) => message.id === fullscreenImageId,
+    );
+
+    if (!image) return null;
+
+    const { uri } = image;
+
+    return (
+      <TouchableHighlight
+        style={styles.fullscreenOverlay}
+        onPress={this.dismissFullscreenImage}
+      >
+        <Image
+          style={styles.fullscreenImage}
+          source={{ uri }}
+        />
+      </TouchableHighlight>
+    );
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -42,6 +175,7 @@ export default class App extends React.Component {
         {this.renderMessageList()}
         {this.renderToolbar()}
         {this.renderInputMethodEditor()}
+        {this.renderFullscreenImage()}
       </View>
     );
   }
